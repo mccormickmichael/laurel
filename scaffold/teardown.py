bf47b0fd75c08f051ce04210c462010304b36dbf
Michael McCormick <michael@thousandleaves.org>
2015-11-27 09:52:39 -0600

    handle stack deleted event gracefully

--- scaffold/teardown.py
+++ scaffold/teardown.py
@@ -4,16 +4,22 @@
 import sys
 import time
 import boto3
+import botocore
 
 cf = boto3.resource('cloudformation')
 
-stack = cf.Stack(sys.argv[1])
+stack_name = sys.argv[1]
+stack = cf.Stack(stack_name)
 
 stack.delete()
 
 while stack.stack_status == 'DELETE_IN_PROGRESS':
     print stack.stack_status, stack.stack_status_reason
     time.sleep(10)
-    stack.reload()
+    try:
+        stack.reload()
+    except botocore.exceptions.ClientError:
+        # okay, we expect to get this when the stack is deleted.
+        pass
     
-print stack.stack_status, stack.stack_status_reason
+print 'stack {} deleted'.format(stack_name)
