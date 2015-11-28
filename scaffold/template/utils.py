19678372709a3db63755b465e16ee5093fad5b9e
Michael McCormick <michael@thousandleaves.org>
2015-11-27 20:37:32 -0600

    Better tag merge implementation

--- scaffold/template/utils.py
+++ scaffold/template/utils.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 
+from troposphere import Tags
+
 def merge_tags(src, dest):
-    """Merge Troposphere Tag objects, replacing any key value in src with matching key values in dest. Dest is modified"""
-    for st in src.tags:
-        match = False
-        for dt in dest.tags:
-            if st['Key'] == dt['Key']:
-                match = True
-                break
-        if not match:
-            dest.tags.append(st)
-    return dest
+    """Merge Troposphere Tag objects. Dest values override src values."""
+    d = {}
+    for st in src.tags + dest.tags:
+        d[st['Key']] = st['Value']
+    return Tags(**d)
+
