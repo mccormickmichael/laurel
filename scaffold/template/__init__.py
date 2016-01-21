8b0fb5017b814eda3e1e02955d6aa0e5aa98619a
Michael McCormick <michael@thousandleaves.org>
2016-01-21 15:53:40 -0600

    better ways to override and convert tag values

--- scaffold/template/__init__.py
+++ scaffold/template/__init__.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python
+
+import troposphere as tp
+import troposphere.autoscaling as autoscaling
+
+def tags_to_dict(taglist):
+    return { t['Key'] : t['Value'] for t in taglist }
+
+def retag(default_tags, **kwargs):
+    """Add/replace tag objects; kwargs overrides existing tag values"""
+    d = tags_to_dict(default_tags.tags)
+    d.update(**kwargs)
+    return tp.Tags(**d)
+
+def asgtag(tags):
+    """Turn regular cloudformation Tags into autoscaling Tags"""
+    d = tags_to_dict(tags.tags)
+    return autoscaling.Tags(**d)
+
+def mergetag(a, b):
+    """Merge Troposphere Tag objects. Dest values override src values."""
+    d = tags_to_dict(a.tags + b.tags)
+    return tp.Tags(**d)
+
