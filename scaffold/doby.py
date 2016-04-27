de18d293a26387c350d76a7c13b40452a43dec71
Michael McCormick <michael@thousandleaves.org>
2016-04-26 21:50:27 -0500

    clean up and tests for doby

--- scaffold/doby.py
+++ scaffold/doby.py
@@ -1,57 +1,26 @@
-def asdot(value):
+def _asdot(value):
     if isinstance(value, list):
-        return Loty(value)
+        return Loby(value)
     if isinstance(value, tuple):
-        return Toty(value)
+        return Toby(value)
     if isinstance(value, dict):
-        return Doty(value)
+        return Doby(value)
     return value
 
-class Toty(tuple):
-    def __getitem__(self, item):
-        return asdot(super(Toty, self).__getitem__(item))
 
-class Loty(list):
+class Toby(tuple):
     def __getitem__(self, item):
-        return asdot(super(Loty, self).__getitem__(item))
+        return _asdot(super(Toby, self).__getitem__(item))
 
-class Doty(dict):
+
+class Loby(list):
+    def __getitem__(self, item):
+        return _asdot(super(Loby, self).__getitem__(item))
+
+
+class Doby(dict):
     def __getattr__(self, attr):
-        return asdot(self[attr])
+        return _asdot(self[attr])
 
     def __getitem__(self, item):
-        return asdot(super(Doty, self).__getitem__(item))
-
-    # __setattr__ could raise exception, since Doty is meant to be read-only
-    # __setitem__ could raise exception, since Doty is meant to be read-only
-
-    
-thingd = { 'one': 'a',
-           'Two': 'b',
-           'three': 'c',
-           'four': 'd',
-           'mega': [
-               'first',
-               { 'foo': 'bar',
-                 'baz': 'qux',
-                 }
-               ],
-           'giga': {
-               'a':1,
-               'b':2,
-               'c':3
-               }
-           }
-
-thingy = Doty(thingd)
-
-print thingy.one
-print thingy['Two']
-print thingy.Two
-print thingy.three
-print thingy['four']
-
-print thingy.mega[0]
-print thingy.mega[1].baz
-print thingy.giga.b
-print thingy['giga'].a
+        return _asdot(super(Doby, self).__getitem__(item))
--- scaffold/tests/test_doby.py
+++ scaffold/tests/test_doby.py
@@ -2,5 +2,33 @@
 
 import unittest
 
+from .. import doby
+
+
 class TestDoby(unittest.TestCase):
-    pass
+
+    def test_attr(self):
+        expected_value = 'value'
+        d = doby.Doby(dict(Attr=expected_value))
+        self.assertEqual(expected_value, d.Attr)
+
+    def test_missing_attr(self):
+        d = doby.Doby()
+        with self.assertRaises(KeyError):
+            d.Thing
+
+    def test_nested_dict(self):
+        d = doby.Doby({'outer': {'inner': 'value'}})
+        self.assertEqual('value', d.outer.inner)
+
+    def test_nested_list(self):
+        d = doby.Doby({'outer': [{'inner': 'value'}, 'two']})
+        self.assertEqual('value', d.outer[0].inner)
+
+    def test_nested_tuple(self):
+        d = doby.Doby({'outer': ({'inner': 'value'}, 'two')})
+        self.assertEqual('value', d.outer[0].inner)
+
+    def test_dict_syntax(self):
+        d = doby.Doby({'outer': {'inner': 'value'}})
+        self.assertEqual('value', d['outer'].inner)
