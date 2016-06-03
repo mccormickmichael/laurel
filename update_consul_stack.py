6e91bd1cf220895497cca0aae751d3ee06f50039
Michael McCormick <michael@thousandleaves.org>
2016-06-03 09:04:48 -0500

    provisional update consul stack

--- update_consul_stack.py
+++ update_consul_stack.py
@@ -6,11 +6,10 @@ import os
 
 import boto3
 
-from scaffold.stack import Outputs
+from scaffold.stack import Parameters, Summary
 from scaffold.stack.operation import StackOperation
 from scaffold.consul.consul_template import ConsulTemplate
 from scaffold.doby import Doby
-
 
 def upload_config(session, bucket_name, key_prefix, base_dir):
     s3 = session.resource('s3')
@@ -29,44 +28,42 @@ def upload_config(session, bucket_name, key_prefix, base_dir):
 def create_stack(args):
     session = boto3.session.Session(profile_name=args.profile)
 
-    cf = session.resource('cloudformation')
-    outputs = Outputs(cf.Stack(args.network_stack_name))
-
-    vpc_id = outputs['VpcId']
-    vpc_cidr = outputs['VpcCidr']
-    private_subnet_ids = outputs.values(lambda k: 'PrivateSubnet' in k)
-    public_subnet_ids = outputs.values(lambda k: 'PublicSubnet' in k)
+    summary = Summary(session, args.stack_name)
+    build_parms = summary.build_parameters()
 
     template = ConsulTemplate(
         args.stack_name,
         region=session.region_name,
-        bucket=args.s3_bucket,
-        vpc_id=vpc_id,
-        vpc_cidr=vpc_cidr,
-        server_subnet_ids=private_subnet_ids,
-        ui_subnet_ids=public_subnet_ids,
-        description=args.desc,
-        server_cluster_size=args.cluster_size,
-        server_instance_type=args.instance_type,
-        ui_instance_type=args.ui_instance_type
+        bucket=build_parms.bucket if args.s3_bucket is None else args.s3_bucket,
+        vpc_id=build_parms.vpc_id,
+        vpc_cidr=build_parms.vpc_cidr,
+        server_subnet_ids=build_parms.server_subnet_ids,
+        ui_subnet_ids=build_parms.ui_subnet_ids,
+        description=summary.description() if args.desc is None else args.desc,
+        server_cluster_size=build_parms.server_cluster_size if args.cluster_size is None else args.cluster_size,
+        server_instance_type=build_parms.server_instance_type if args.instance_type is None else args.instance_type,
+        ui_instance_type=build_parms.ui_instance_type if args.ui_instance_type is None else args.ui_instance_type
     )
     template.build_template()
     template_json = template.to_json()
 
     results = {'template': template_json}
-    basedir = os.path.dirname(inspect.getfile(ConsulTemplate))
-
-    upload_config(session, args.s3_bucket, 'scaffold', basedir)  # TODO: replace 'scaffold' with args.key_prefix
-
     if args.dry_run:
         return Doby(results)
 
+    basedir = os.path.dirname(inspect.getfile(ConsulTemplate))
+    upload_config(session, args.s3_bucket, 'scaffold', basedir)  # TODO: replace 'scaffold' with args.key_prefix
+
+    cf = session.resource('cloudformation')
+    current_parms = Parameters(cf.Stack(args.stack_name))
+
     stack_parms = {
-        ConsulTemplate.CONSUL_KEY_PARAM_NAME: args.consul_key
+        ConsulTemplate.CONSUL_KEY_PARAM_NAME:
+        current_parms[ConsulTemplate.CONSUL_KEY_PARAM_NAME] if args.consul_key is None else args.consul_key
     }
 
-    creator = StackOperation(session, args.stack_name, template_json, args.s3_bucket, args.s3_key_prefix)
-    stack = creator.create(stack_parms)
+    updater = StackOperation(session, args.stack_name, template_json, args.s3_bucket, args.s3_key_prefix)
+    stack = updater.update(stack_parms)
     results['stack_id'] = stack.stack_id
     results['stack_status'] = stack.stack_status
     results['stack_status_reason'] = stack.stack_status_reason
@@ -74,10 +71,6 @@ def create_stack(args):
     return Doby(results)
 
 
-default_desc = 'Consul Stack'
-default_instance_type = 't2.micro'
-default_ui_instance_type = 't2.micro'
-default_cluster_size = 3
 default_s3_bucket = 'thousandleaves-us-west-2-laurel-deploy'
 default_s3_key_prefix = 'scaffold/templates'
 default_profile = 'default'
@@ -87,23 +80,23 @@ def get_args():
     ap = argparse.ArgumentParser(description='Create a CloudFormation stack hosting a Consul cluster')
     ap.add_argument('stack_name',
                     help='Name of the Consul stack to create')
-    ap.add_argument('network_stack_name',
-                    help='Name of the network stack')
 
-    ap.add_argument('--desc', default=default_desc,
+    ap.add_argument('--desc',
                     help='Stack description. Strongy encouraged.')
-    ap.add_argument('--consul-key', required=True,
-                    help='Name of the key pair used to access the Consul cluster instances. Required.')
-    ap.add_argument('--instance-type', default=default_instance_type,
-                    help='Instance type for the Consul servers. Default: {}'.format(default_instance_type))
-    ap.add_argument('--ui-instance-type', default=default_ui_instance_type,
-                    help='Instance type for the Consul UI servers. Default: {}'.format(default_ui_instance_type))
-    ap.add_argument('--cluster_size', default=default_cluster_size,
-                    help='Number of instances in the Consul cluster. Should be an odd number > 1. Default: {}'.format(default_cluster_size))
+    ap.add_argument('--consul-key',
+                    help='Name of the key pair used to access the Consul cluster instances.')
+    ap.add_argument('--instance-type',
+                    help='Instance type for the Consul servers.')
+    ap.add_argument('--ui-instance-type',
+                    help='Instance type for the Consul UI servers.')
+    ap.add_argument('--cluster_size',
+                    help='Number of instances in the Consul cluster. Should be an odd number > 1.')
+
     ap.add_argument('--s3-bucket', default=default_s3_bucket,
                     help='Name of the S3 bucket to which stack template files are uploaded. Default: {}'.format(default_s3_bucket))
     ap.add_argument('--s3-key-prefix', default=default_s3_key_prefix,
                     help='Prefix to use when uploading stack template files to the bucket. Default: {}'.format(default_s3_key_prefix))
+
 
     ap.add_argument('--profile', default=default_profile,
                     help='AWS Credential and Config profile to use. Default: {}'.format(default_profile))
@@ -111,10 +104,9 @@ def get_args():
                     help='Echo the parameters to be used to create the stack; take no action')
     return ap.parse_args()
 
-
 if __name__ == '__main__':
     args = get_args()
-    results = create_stack(args)
+    results = update_stack(args)
     if args.dry_run:
         print results.template
     else:
@@ -122,3 +114,4 @@ if __name__ == '__main__':
         print 'STATUS: ', results.stack_status
         if results.stack_status_reason is not None:
             print 'REASON: ', results.stack_status_reason
+
