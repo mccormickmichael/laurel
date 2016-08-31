a1d4501bc835a00035cfd198483ab3a357e639eb
Michael McCormick <michael@thousandleaves.org>
2016-08-31 13:37:34 -0500

    Revert "add policy definitions to IAM template" locating non-stack policy arns has no palatable options at this time.
    This reverts commit 76bb671bc8bae81d9ccf6d294128a26cac74d58c.

--- scaffold/iam/cf_template.py
+++ scaffold/iam/cf_template.py
@@ -1,12 +1,9 @@
-import json
-import os.path
+
 import troposphere.cloudtrail as ct
-import troposphere.iam as iam
 import troposphere.s3 as s3
 import troposphere as tp
 
 from scaffold.template import TemplateBuilder
-from . import discover_policy_files, load_policy_file
 
 
 class IAMTemplate(TemplateBuilder):
@@ -16,21 +13,14 @@ class IAMTemplate(TemplateBuilder):
     OUTPUT_NAME_BUCKET = 'CloudTrailBucket'
     OUTPUT_NAME_TRAIL = 'Trail'
 
-    POLICY_DIR_NAME = 'policies'
-    USER_FILE_NAME = 'users.yml'
-    GROUP_FILE_NAME = 'groups.yml'
-    ROLE_FILE_NAME = 'roles.yml'
-
     def __init__(self, name,
                  s3_bucket_name,
                  s3_path_prefix='',
-                 iam_doc_dir='.',
                  description='[REPLACE ME]',
                  logging_enabled=False):
         super(IAMTemplate, self).__init__(name, description, IAMTemplate.BUILD_PARM_NAMES)
         self.s3_bucket_name = s3_bucket_name
         self.s3_path_prefix = s3_path_prefix
-        self.iam_doc_dir = iam_doc_dir
         self.logging_enabled = logging_enabled
 
     def internal_add_mappings(self):
@@ -39,16 +29,6 @@ class IAMTemplate(TemplateBuilder):
     def internal_build_template(self):
         self._create_cloudtrail_bucket()
         self._create_cloudtrail()
-        self._create_policies()
-
-    def _create_policies(self):
-        policy_file_mapping = discover_policy_files(self._policy_dir())
-        for policy_name, policy_file in policy_file_mapping.items():
-            policy_doc = load_policy_file(policy_file)
-            policy = iam.ManagedPolicy(policy_name,
-                                       PolicyDocument=policy_doc)
-            self.add_resource(policy)
-            self.output_ref(IAMTemplate._policy_res_name(policy_name), policy)
 
     def _create_cloudtrail_bucket(self):
         lc = s3.LifecycleConfiguration(Rules=[s3.LifecycleRule(ExpirationInDays=30,
@@ -61,7 +41,7 @@ class IAMTemplate(TemplateBuilder):
                            # DeletionPolicy='Retain',
                            Tags=self.default_tags)
 
-        policy = s3.BucketPolicy(IAMTemplate._policy_res_name(self.OUTPUT_NAME_BUCKET),
+        policy = s3.BucketPolicy(self.OUTPUT_NAME_BUCKET + 'Policy',
                                  Bucket=tp.Ref(bucket),
                                  PolicyDocument=self._create_bucket_policy(),
                                  DependsOn=bucket.title)
@@ -111,13 +91,6 @@ class IAMTemplate(TemplateBuilder):
                 }
             ]
         }
-
-    @staticmethod
-    def _policy_res_name(policy_name):
-        return policy_name + 'Policy'
-
-    def _policy_dir(self):
-        return os.path.join(self.iam_doc_dir, IAMTemplate.POLICY_DIR_NAME)
 
 if __name__ == '__main__':
     template = IAMTemplate('testing', 'test-bucket')
--- tests/iam/test_cf_template.py
+++ tests/iam/test_cf_template.py
@@ -1,23 +1,13 @@
-import inspect
-import os.path
 import unittest
 
-import troposphere.iam as iam
-
 from scaffold.iam.cf_template import IAMTemplate
-from scaffold.iam import discover_policy_files
+
 
 class TestIAMTemplate(unittest.TestCase):
 
     def setUp(self):
-        self.iam_doc_dir = os.path.dirname(inspect.getfile(self.__class__))
-        self.template = IAMTemplate('TestTemplate', 'test-bucket',
-                                    iam_doc_dir=self.iam_doc_dir)
+        self.template = IAMTemplate('TestTemplate', 'test-bucket')
         self.template.build_template()
-
-    def test_policy_resource_name(self):
-        resource_name = IAMTemplate._policy_res_name('Test')
-        self.assertEqual('TestPolicy', resource_name)
 
     def test_should_add_bucket_as_output(self):
         output = self.template.template.outputs.get(IAMTemplate.OUTPUT_NAME_BUCKET)
@@ -36,18 +26,5 @@ class TestIAMTemplate(unittest.TestCase):
         self.assertIsNotNone(res)
 
     def test_should_add_bucket_policy_as_resource(self):
-        res = self.template.template.resources.get(IAMTemplate._policy_res_name(IAMTemplate.OUTPUT_NAME_BUCKET))
+        res = self.template.template.resources.get(IAMTemplate.OUTPUT_NAME_BUCKET + 'Policy')
         self.assertIsNotNone(res)
-
-    def test_should_add_policy_files_as_managed_policy_files(self):
-        policy_files = discover_policy_files(self.template._policy_dir())
-        for policy_name in policy_files.keys():
-            res = self.template.template.resources.get(policy_name)
-            self.assertIsNotNone(res)
-            self.assertIsInstance(res, iam.ManagedPolicy)
-
-    def test_should_add_policy_names_as_output(self):
-        policy_files = discover_policy_files(self.template._policy_dir())
-        for policy_name in policy_files.keys():
-            output = self.template.template.outputs.get(IAMTemplate._policy_res_name(policy_name))
-            self.assertIsNotNone(output)
